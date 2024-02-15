34e46fd0732483f505fbefe2f4324c08ab38226e
BrettAF <bford5@uccs.edu>
2024-02-15 15:06:15 -0700

    posted with an image

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+# Render index.html
+   return render( request, 'portfolio_app/index.html')
+
+def stub(request):
+   return render(request,"portfolio_app/stub.html")
--- startup.md
+++ startup.md
@@ -1,3 +1,6 @@
 cd Documents/cs3300/portfolio
 source ~/Documents/cs3300/portfolio/djvenv/Scripts/activate
 
+start the server with 
+python manage.py runserver
+
